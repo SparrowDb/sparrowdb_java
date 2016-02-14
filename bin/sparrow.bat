a9b9f1bcf042f5947fa3f3e48d9b20c8b090e286
mausimag <mmmauriciosilvam@gmail.com>
2016-02-13 23:07:15 -0200

    new jar compilation method

--- bin/sparrow.bat
+++ bin/sparrow.bat
@@ -5,6 +5,7 @@ if NOT DEFINED SPARROW_HOME set SPARROW_HOME=%CD%
 IF /i "%1" == "build" GOTO build
 IF /i "%1" == "start" GOTO start
 IF /i "%1" == "" GOTO start
+IF /i "%1" == "datatool" GOTO datatool
 
 :build
 mvn clean install
@@ -20,7 +21,7 @@ set JAVA_OPTS=-Xmx2G^
  -XX:+CMSParallelRemarkEnabled^
  -XX:SurvivorRatio=8
  
-if EXIST target/sparrow-1.0-SNAPSHOT-jar-with-dependencies.jar (
+if EXIST target/sparrow-1.0-SNAPSHOT.jar (
 	goto exec
 ) else (
 	echo [ERRO] Sparrow main file does not exists, please try to run "sparrow build"
@@ -28,8 +29,14 @@ if EXIST target/sparrow-1.0-SNAPSHOT-jar-with-dependencies.jar (
 
 goto end
 
+:datatool
+set SPARROW_MAIN=org.sparrow.tools.DataExtract
+goto exec
+goto end
+
 :exec
-"%JAVA_HOME%\bin\java" %JAVA_OPTS% -jar target/sparrow-1.0-SNAPSHOT-jar-with-dependencies.jar "%SPARROW_MAIN%"
+set SPARROW_CLASSPATH=target/lib/*
+"%JAVA_HOME%\bin\java" %JAVA_OPTS% -cp "target/sparrow-1.0-SNAPSHOT.jar;%SPARROW_CLASSPATH%" %SPARROW_MAIN%
 goto end
 
 :end
